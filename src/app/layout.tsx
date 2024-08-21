7b64a34dc2d53e58206b2123cd9950679ff8d79c
LeoQuark <leonardo.penaf@utem.cl>
2024-08-20 23:49:53 -0400

    feat:add structured folders and pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
+import { monserrat } from "../components/ui/fonts"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
